c9ccafc269f7990c0854d520c5eafa5162a1c414
César Guerra <warderer@gmail.com>
2025-10-20 20:45:00 -0600

    💄 Add basic template

--- 01-react-props/src/main.jsx
+++ 01-react-props/src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App saludo='Hola' mensaje='G42A, soy un Prop' />
-    <App saludo='Hi' mensaje='G42A, Im a Prop' />
+    <App />
   </StrictMode>,
 )
