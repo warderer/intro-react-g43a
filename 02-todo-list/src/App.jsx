8623119d7f25b39773f3e484974d43a2a4874549
César Guerra <warderer@gmail.com>
2025-10-22 19:48:21 -0600

    💄 ToDoList base UI

--- 02-todo-list/src/App.jsx
+++ 02-todo-list/src/App.jsx
@@ -3,7 +3,15 @@ import './App.css'
 function App () {
   return (
     <>
-
+      <h1>Todo List</h1>
+      <input text='text' placeholder='Escribe una tarea...' />
+      <button>Añadir</button>
+      {/* ul>li{Tarea $}*3>button{Eliminar} */}
+      <ul>
+        <li>Tarea 1<button>Eliminar</button></li>
+        <li>Tarea 2<button>Eliminar</button></li>
+        <li>Tarea 3<button>Eliminar</button></li>
+      </ul>
     </>
   )
 }
