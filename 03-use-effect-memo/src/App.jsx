82dd7513b3e2e919793252b075e6beef2e5d09d2
César Guerra <warderer@gmail.com>
2025-10-27 19:35:49 -0600

    ✨ Add userList, states, fetch, and useEffect

--- 03-use-effect-memo/src/App.jsx
+++ 03-use-effect-memo/src/App.jsx
@@ -1,6 +1,39 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
+function App () {
+  const listaDeUsuarios = [
+    { id: 1, nombre: 'Angela', pais: 'Argentina' },
+    { id: 2, nombre: 'Karla', pais: 'Colombia' },
+    { id: 3, nombre: 'Lizbeth', pais: 'México' },
+    { id: 4, nombre: 'Maycol', pais: 'Perú' },
+    { id: 5, nombre: 'Melani', pais: 'Chile' },
+    { id: 6, nombre: 'Pedro', pais: 'Venezuela' },
+  ]
+
+  // Estados para manejar usuarios
+  const [usuarios, setUsuarios] = useState([])
+  const [busqueda, setBusqueda] = useState('')
+  const [cargando, setCargando] = useState(true)
+
+  // Función que simula la llamada a la API
+  const fetchUsuarios = () => {
+    return new Promise((resolve) => {
+      console.log('Cargando usuarios...')
+      setTimeout(() => {
+        resolve(listaDeUsuarios)
+      }, 2500)
+    })
+  }
+
+  // useEffect Lo usamos para cargar los datos (en este caso la lista de usuarios), una vez que el componente se monta (inicia por primera vez)
+  useEffect(() => {
+    setCargando(true)
+    fetchUsuarios().then((data) => {
+      setUsuarios(data)
+      setCargando(false)
+    })
+  }, [])
 
   return (
     <>
