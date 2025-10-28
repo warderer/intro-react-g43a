be7e1faf365d3e58805e193ebb09a16beff80a60
César Guerra <warderer@gmail.com>
2025-10-27 19:47:39 -0600

    ✨ Add filter function amd style object

--- 03-use-effect-memo/src/App.jsx
+++ 03-use-effect-memo/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import './App.css'
 
 function App () {
@@ -15,6 +15,8 @@ function App () {
   const [usuarios, setUsuarios] = useState([])
   const [busqueda, setBusqueda] = useState('')
   const [cargando, setCargando] = useState(true)
+  // Estado para manejar el tema (claro/oscuro)
+  const [tema, setTema] = useState('light')
 
   // Función que simula la llamada a la API
   const fetchUsuarios = () => {
@@ -34,6 +36,22 @@ function App () {
       setCargando(false)
     })
   }, [])
+
+  // useMemo: Lo usamos para filtrar los usuarios cada vez que cambia el estado de busqueda de los usuarios.
+
+  const usuariosFiltrados = useMemo(() => {
+    return usuarios.filter((usuario) =>
+      usuario.nombre.toLowerCase().includes(busqueda.toLowerCase())
+    )
+  }, [usuarios, busqueda])
+
+  const estilo = {
+    backgroundColor: tema === 'light' ? '#fff' : '#333',
+    color: tema === 'light' ? '#000' : '#fff',
+    padding: '20px',
+    borderRadius: '5px',
+    transition: 'all 0.3s ease'
+  }
 
   return (
     <>
