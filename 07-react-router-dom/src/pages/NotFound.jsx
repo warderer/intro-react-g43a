4411e77a58e91ba4f05ee6b507f6e082decdd143
César Guerra <warderer@gmail.com>
2025-11-10 21:25:51 -0600

    ✨ Update NotFound with Link and Image

--- 07-react-router-dom/src/pages/NotFound.jsx
+++ 07-react-router-dom/src/pages/NotFound.jsx
@@ -1,6 +1,14 @@
+import { Link } from 'react-router-dom'
+import image404 from '../assets/404-rick-and-morty.png'
+
 const NotFound = () => {
   return (
-    <h1>NotFound</h1>
+    <div className='page-content'>
+      <h1>404 - Página No Encontrada</h1>
+      <p>La página que buscas no existe. ¡Wubba Lubba Dub Dub!</p>
+      <img src={image404} alt='Rick and Morty 404' className='not-found-image' />
+      <Link to='/' className='btn-back'>Volver al Inicio</Link>
+    </div>
   )
 }
 export default NotFound
