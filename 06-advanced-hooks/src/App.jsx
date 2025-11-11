a959dd2a8707cc69ba9544e21ff6445e6745ce89
César Guerra <warderer@gmail.com>
2025-11-10 20:37:13 -0600

    ✨ App implementation

--- 06-advanced-hooks/src/App.jsx
+++ 06-advanced-hooks/src/App.jsx
@@ -1,11 +1,56 @@
+import { useReducer, useCallback } from 'react'
+import { formReducer, initialState } from './reducers/formReducer'
+import Step1 from './components/Step1'
+import Step2 from './components/Step2'
+import Success from './components/Success'
 import './App.css'
 
-function App() {
+function App () {
+  const [state, dispatch] = useReducer(formReducer, initialState)
+
+  // Funciones con useCallBack para evitar re-rendizacion innecesaria
+  const nextStep = useCallback(() => dispatch({ type: 'NEXT_STEP' }), [])
+  const prevStep = useCallback(() => dispatch({ type: 'PREV_STEP' }), [])
+  const handleFieldChange = useCallback((e) => {
+    const { name, value } = e.target
+    dispatch({
+      type: 'UPDATE_TEXT',
+      payload: { name, value }
+    })
+  }, [])
+  const reset = useCallback(() => dispatch({ type: 'RESET' }), [])
+
+  // Renderizado condicional basado en el paso actual (step)
+  const renderStep = () => {
+    switch (state.step) {
+      case 1:
+        return (
+          <Step1
+            formData={state.formData}
+            handleFieldChange={handleFieldChange}
+            nextStep={nextStep}
+          />
+        )
+      case 2:
+        return (
+          <Step2
+            formData={state.formData}
+            handleFieldChange={handleFieldChange}
+            nextStep={nextStep}
+            prevStep={prevStep}
+          />
+        )
+      case 3:
+        return <Success formData={state.formData} reset={reset} />
+      default:
+        return <div>Paso desconocido</div>
+    }
+  }
 
   return (
-    <>
-
-    </>
+    <div className='form-container'>
+      {renderStep()}
+    </div>
   )
 }
 
