f68142d9e238c3258745c88aa56b3f1c5ae18ad6
César Guerra <warderer@gmail.com>
2025-11-10 21:23:26 -0600

    ✨ Update Home with Info + Link component

--- 07-react-router-dom/src/pages/Home.jsx
+++ 07-react-router-dom/src/pages/Home.jsx
@@ -1,6 +1,19 @@
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div className='page-content'>
+      <h1>Aprendiendo React Router</h1>
+      <p>
+        Esta es una aplicación de demostración que utiliza la API de Rick and Morty para ilustrar las funcionalidades de <code>react-router-dom</code>.
+      </p>
+      <p>
+        Navega a la sección de personajes para comenzar.
+      </p>
+      <Link to='/characters' className='btn-back'>
+        Ir a personajes
+      </Link>
+    </div>
   )
 }
 export default Home
