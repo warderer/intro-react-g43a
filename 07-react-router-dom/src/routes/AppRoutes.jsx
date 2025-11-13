978abc224973b9aba02c25406b74ce396df69dd7
César Guerra <warderer@gmail.com>
2025-11-12 19:10:16 -0600

    ✨ Update AppRoutes with Secret protected route

--- 07-react-router-dom/src/routes/AppRoutes.jsx
+++ 07-react-router-dom/src/routes/AppRoutes.jsx
@@ -3,13 +3,25 @@ import Home from '../pages/Home'
 import Characters from '../pages/Characters'
 import CharacterDetail from '../pages/CharacterDetail'
 import NotFound from '../pages/NotFound'
+import Secret from '../pages/Secret'
+import RequireAuth from '../auth/RequireAuth'
 
-const AppRoutes = () => {
+const AppRoutes = ({ estoyLoggeado }) => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/characters' element={<Characters />} />
       <Route path='/characters/:id' element={<CharacterDetail />} />
+
+      {/* Ruta Protegida */}
+      <Route
+        path='/secret' element={
+          <RequireAuth isAuth={estoyLoggeado}>
+            <Secret />
+          </RequireAuth>
+        }
+      />
+
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
