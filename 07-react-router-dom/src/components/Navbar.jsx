6344d72a96e5b0d9892f5e98a98e90d5d57b589c
César Guerra <warderer@gmail.com>
2025-11-12 19:33:42 -0600

    ✨ Updated Navbar with Auth conditional rendering

--- 07-react-router-dom/src/components/Navbar.jsx
+++ 07-react-router-dom/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ estoyLoggeado, onLogin, onLogout }) => {
   return (
     <nav className='navbar'>
       <ul>
@@ -10,7 +10,35 @@ const Navbar = () => {
         <li>
           <NavLink to='/characters'>Personajes</NavLink>
         </li>
+        {
+          estoyLoggeado && (
+            <li>
+              <NavLink to='/secret'>Sección Secreta</NavLink>
+            </li>
+          )
+        }
       </ul>
+
+      <div className='auth-buttons'>
+        {estoyLoggeado
+          ? (
+            <button
+              onClick={onLogout}
+              className='btn btn-link'
+            >
+              Logout
+            </button>
+            )
+          : (
+            <button
+              onClick={onLogin}
+              className='btn btn-link'
+            >
+              Login
+            </button>
+            )}
+      </div>
+
     </nav>
   )
 }
